ef1704b1393ec93d23fd9c98ee438e0dcb56e1cf
Jaydatt <124486498+jaydattpatel@users.noreply.github.com>
2024-02-29 13:50:05 +0530

    Delete Programming with JavaScript by Meta (Coursera) directory

--- Programming with JavaScript by Meta (Coursera)/Week-4/1. Jest Test/Project Submitted/learn/jest-testing/timesTwo.js
+++
@@ -1,6 +0,0 @@
-// Task 1: Code the timesTwo function declaration
-function timesTwo(value) {
-    return (value * 2);
-}
-// Task 2: Export the timesTwo function as a module
-module.exports = timesTwo;
