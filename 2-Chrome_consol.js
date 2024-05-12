88b4e4f43f5bf2f1681192d45ce2fe170ecd26a2
Jaydatt <124486498+jaydattpatel@users.noreply.github.com>
2024-05-12 14:34:49 +0530

    Delete 2-Chrome_consol.js

--- 2-Chrome_consol.js
+++
@@ -1,7 +0,0 @@
-/**
- * Author : Jaydatt Patel
- */
-console.log("Hello, World");
-console.log("%cHello, World", "color: blue; font-size: 40px");
-console.log("Hello " + "there, " + "World");//Not space add automatically
-console.log("Hello","there,","World");//space add automatically
