0e57ff2849a9fdcebc20b0c953eca695fafd4141
Jaydatt <124486498+jaydattpatel@users.noreply.github.com>
2024-05-30 20:01:43 +0530

    Update 052-Async-await-function.js

--- 052-Async-await-function.js
+++ 052-Async-await-function.js
@@ -1,7 +1,25 @@
 /*
 Author : Jaydatt Patel
 
-Async function : The async function declaration creates a binding of a new async function to a given name. The await keyword is permitted within the function body, enabling asynchronous, promise-based behavior to be written in a cleaner style and avoiding the need to explicitly configure promise chains.
+Async function, await: 
+The async function declaration creates a binding of a new async function to a given name. The await keyword is permitted within the function body, enabling asynchronous, promise-based behavior to be written in a cleaner style and avoiding the need to explicitly configure promise chains.
+
+Note: We can use await with function only in async function. 
+
+method: 
+
+  function hold{
+      return Promise((resolve,reject)=>{
+          setTimeout(()=>{
+              ......
+              resolve(arg)
+          },time)
+      });
+  }
+  async function fun_name(){
+      let result = await hold()  //wait to complete function
+      console.log(result)
+  }
 
 */
 
@@ -12,16 +30,13 @@ function hold() {
           resolve('resolved');
         }, 2000);
     });
-  }
+}
   
-  async function asyncCall() {
+async function asyncCall() {
       console.log('calling');
       const result = await hold();
       console.log(result);
-  }
-  
-  asyncCall();
-      
+}
 
+asyncCall();
 
-  
