cc2fea68de9846d5b1ee8f308b03f067a778bc92
Jaydatt <124486498+jaydattpatel@users.noreply.github.com>
2024-06-12 20:15:45 +0530

    Add files via upload

---
+++ Import - Export Modules/ES6 - Import-Export  Module/Main.mjs
@@ -0,0 +1,44 @@
+/*
+author : Jaydatt Patel
+
+ES6 Module : 
+Modules are a fundamental concept in modern JavaScript development, allowing developers to organize code into separate files or modules. Each module encapsulates its variables, functions, and classes, avoiding naming conflicts and polluting the global scope. This modularity promotes code reusability, maintainability, and scalability in large-scale projects.
+
+File Name must be with .mjs extension : 
+    moduleFile.mjs
+    MainFile.mjs
+
+*/
+
+//single import
+// import {add} from './Arithmetic.mjs';  
+
+//multiple import
+// import {add,sub} from './Arithmetic.mjs'; 
+
+//multiple import with alise name
+// import {add as addition, sub as subtraction} from './Arithmetic.mjs'; 
+
+//import default only without {}
+// import info from './Arithmetic.mjs';
+
+//import default only with alise name and without {}
+// import infomation from './Arithmetic.mjs';
+
+//default and multiple import
+// import info, {add,sub} from './Arithmetic.mjs'; 
+
+//default and multiple import with alise name
+// import infomation, {add as additon, sub as subtraction} from './Arithmetic.mjs'; 
+
+//all import
+
+// require('path') to import
+import * as operation from './Arithmetic.mjs'; 
+
+operation.add(2,3);
+operation.sub(10,7);
+operation.mul(4,3);
+//  operation.div(20,2); //error: not fun. need to export to use
+operation.default();
+
